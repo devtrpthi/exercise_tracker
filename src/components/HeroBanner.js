159cc83f9fe8974be05a9d051bc93a00d947fd33
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-29 17:49:50 +0530

    home page content

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Stack, Typography, typography } from "@mui/material";
+
+function HeroBanner() {
+    return(
+        <Box>
+            <Typography color="#FF2625"
+            fontWeight="660" fontsize="26px">
+                Fitness Club
+            </Typography>
+        </Box>
+    );
+}
+
+export default HeroBanner;
