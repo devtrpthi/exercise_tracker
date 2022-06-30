547065e2d037bf11f3e23ed7367bf0ed1cde0f68
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-06-30 17:19:25 +0530

    changes

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -3,6 +3,7 @@ import {Box, Button, Stack, TextField,Typography} from '@mui/material';
 import { borderRadius } from "@mui/material";
 
 function SearchExercises() {
+    const [search, setsearch] = useState('');
     return(
         <Stack alignItems= 'center' mt='37px'
         justifyContent= 'center' p='20px'>
