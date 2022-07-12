3842411d1d628e6354445e643374639ecc364c44
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-12 20:50:29 +0530

    modified

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/system";
+
+import {exerciseOptions, fetchData} from './utils/fetchData';
+
+
 
 function ExerciseDetail() {
     return(
-        <div>
-            exercise
-        </div>
+        <Box>
+            <Detail/>
+            <ExerciseVideos/>
+            <SimilarExercises/>
+        </Box>
     );
 }
 
