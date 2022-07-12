0315d14ea15ab442c66073e630d3dd26e74c7d8f
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-12 15:06:48 +0530

    item-map

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from '../assets/icons/gym.png';
 
-function BodyPart() {
+function BodyPart({item, bodyPart, setBodyPart}) {
     return(
-        <div>
-            BodyPart
-        </div>
+       <Stack
+       type='button' alignItems='center' justifyContent='center' className="bodyPart-card"
+       sx={
+            bodyPart == item ? {
+                borderTop: '4px solid #ff2625',
+                backgroundColor: '#fff',
+            } : ''
+       }
+       >
+        <img src={Icon} alt='dumbbell' style={{ width:'40px', height: '40px'}}/>
+       </Stack>
     );
 }
 
