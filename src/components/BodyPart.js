c4a99460bc6fddcce2013a71c4c14506213c16d4
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-12 16:45:26 +0530

    window-scroll

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -14,8 +14,13 @@ function BodyPart({item, bodyPart, setBodyPart}) {
                 heigth: '280px',
                 cursor: 'pointer', gap: '47px' 
        }}
+       onClick={()=>{
+        setBodyPart(item);
+        window.scrollTo({top:1800, left: 100,behavior: 'smooth'})
+       }}
        >
         <img src={Icon} alt='dumbbell' style={{ width:'40px', height: '40px'}}/>
+        <Typography fontSize='24px' fontWeight='bold' color='#3A1212' textTransform='capitalize'>{item}</Typography>
        </Stack>
     );
 }
