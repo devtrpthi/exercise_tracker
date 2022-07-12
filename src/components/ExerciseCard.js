7f54398474fd0e7dd9e160da6932dd2ac21183f3
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-12 18:43:25 +0530

    paginate-transform

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -18,7 +18,7 @@ function ExerciseCard({exercise}) {
                     {exercise.target}
                 </Button>
             </Stack>
-            <Typography ml='21px' color='#000' fontWeight='bold' mt='11px' pb='10px' textTransform='capitalize'>
+            <Typography ml='21px' color='#000' fontWeight='bold' mt='11px' pb='10px' textTransform='capitalize' fontSize= '22px'>
                 {exercise.name}
             </Typography>
         </Link>
