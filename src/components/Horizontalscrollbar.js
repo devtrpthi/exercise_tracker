221268519e484aa049cb0d7fd248843999951e3d
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-12 15:10:39 +0530

    modified

--- src/components/Horizontalscrollbar.js
+++ src/components/Horizontalscrollbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Box } from "@mui/material";
 import BodyPart from "./BodyPart";
 
-function HorizontalScrollbar({data}) {
+function HorizontalScrollbar({data, bodyParts, setBodyPart, bodyPart}) {
     return(
         <div>
             {data.map((item) => (
