90c1fc402e594f0f50324aacaf530b9279df1fa4
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-12 22:32:43 +0530

    exercise-details

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,15 +1,18 @@
 import React,{useEffect,useState} from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/system";
-
-import {exerciseOptions, fetchData} from './utils/fetchData';
+import {exerciseOptions, fetchData} from '../utils/fetchData';
+import Details from "../components/Details";
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 
 
 function ExerciseDetail() {
+    const[exerciseDetail, setExerciseDetail] = useState();
     return(
         <Box>
-            <Detail/>
+            <Details exerciseDetail={exerciseDetail} />
             <ExerciseVideos/>
             <SimilarExercises/>
         </Box>
