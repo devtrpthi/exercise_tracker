11e749e5157f94a7c5f6a6c79c48d64f36b0a271
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-13 12:52:11 +0530

    equipment-loader

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -6,10 +6,17 @@ import Loader from "./Loader";
 function SimilarExercises({targetMuscleExercises,equipmentExercises}) {
     return(
        <Box sx={{mt: {lg:'100px' ,xs: '0'}}}>
-        <Typography variant='h3'>Exercises that target same muscle group</Typography>
+        <Typography variant='h3' mb={5}>Exercises that target same muscle group</Typography>
         <Stack direction='row' sx={{p:'2', position: 'relative'}}>
             {targetMuscleExercises.length ?
              <HorizontalScrollbar data={targetMuscleExercises}/>
+             :<Loader/>}
+        </Stack>
+
+        <Typography variant='h3' mb={5}>Exercises that uses the same equipments</Typography>
+        <Stack direction='row' sx={{p:'2', position: 'relative'}}>
+            {equipmentExercises.length ?
+             <HorizontalScrollbar data={equipmentExercises}/>
              :<Loader/>}
         </Stack>
        </Box>
