152ca7a5515d4f41751f980a4476d6f68f164321
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-13 11:58:23 +0530

    channel-thumbnail-card

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,10 +1,40 @@
 import React from "react";
+import { Box,Stack,Typography } from "@mui/material";
 
-function ExerciseVideos() {
+function ExerciseVideos({ exerciseVideos,name }) {
+    console.log(exerciseVideos);
+    if(!exerciseVideos.length) return 'Loading..';
     return(
-        <div>
-            exercise videos
-        </div>
+       <Box
+       sx={{marginTop: {lg:'200px', xs: '20px'}}} p='20px'>
+        <Typography variant='h3' marginBottom='33px'>
+            Watch <span style={{color: '#ff2625', textTransform:'capitalize'}}> {name}</span> exercise videos
+        </Typography>
+        <Stack justifyContent='flex-start' flexWrap='wrap' alignItems='center'
+        sx={{
+            flexDirection: {lg:'row'},
+            gap: {lg:'110px', xs: '0'}
+        }} >
+            {exerciseVideos?.slice(0,6).map((item,index) => (
+                <a
+                key={index}
+                className='exercise-Video'
+                href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+                target='_blank'
+                rel='noreferrer'>
+                    <img src={item.video.thumbnails[0].url} alt={item.video.title} style={{ borderTopLeftRadius: '20px', borderTopRightRadius: '20px' }}/>
+                    <Box>
+                        <Typography sx={{ fontSize: { lg: '28px', xs: '18px' } }} fontWeight={600} color='#000'>
+                            {item.video.title}
+                        </Typography>
+                        <Typography fontSize="14px" color='#000'>
+                            {item.video.channelName}
+                        </Typography>
+                    </Box>
+                </a>
+            ))}
+        </Stack>
+       </Box>
     );
 }
 
