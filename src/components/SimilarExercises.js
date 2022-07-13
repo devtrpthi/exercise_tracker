cc8e99008933b87111e67ceaf503d5220aa23ff0
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-13 12:30:57 +0530

    exercises-card-scroll

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Box, Stack, Typograpgy, Typography } from "@mui/material";
+import HorizontalScrollbar from './Horizontalscrollbar';
+import Loader from "./Loader";
 
-function SimilarExercises() {
+function SimilarExercises({targetMuscleExercises,equipmentExercises}) {
     return(
-        <div>
-            similar exercises
-        </div>
+       <Box sx={{mt: {lg:'100px' ,xs: '0'}}}>
+        <Typography variant='h3'>Exercises that target same muscle group</Typography>
+        <Stack direction='row' sx={{p:'2', position: 'relative'}}>
+            {targetMuscleExercises.length ?
+             <HorizontalScrollbar data={targetMuscleExercises}/>
+             :<Loader/>}
+        </Stack>
+       </Box>
     );
 }
 
