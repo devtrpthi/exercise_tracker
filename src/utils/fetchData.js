e3c09a8899b182966985d555145b5d305af8c853
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-13 11:59:40 +0530

    fetch-youtube

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -4,16 +4,16 @@ export const exerciseOptions = {
     url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
     headers: {
       'X-RapidAPI-Key': '43a0976045mshd0881ed128d4cf3p1e3e18jsn1e3441ef6442',
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    },
   };
 
  export const youtubeOptions = {
     method: 'GET',
     headers: {
       'X-RapidAPI-Key': '43a0976045mshd0881ed128d4cf3p1e3e18jsn1e3441ef6442',
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-    }
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    },
   };
 
 
@@ -22,4 +22,4 @@ export const fetchData = async(url,options) => {
     const data = await response.json();
 
     return data;
-}
+};
